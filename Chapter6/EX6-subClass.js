e3ad98b760bb553534ebc5ee163cf18ebd8ad8e9
KwonYH <yhoung11@gmail.com>
2022-09-29 23:01:22 +0900

    subClass 생성자호출

--- Chapter6/EX6-subClass.js
+++ Chapter6/EX6-subClass.js
@@ -4,7 +4,15 @@ function subClass(obj) {
     var parent = this;
     var F = function () { };
 
-    var child = function () { };
+    var child = function () {
+        var parent = child.parent;
+        if (parent._init) {
+            parent._init.apply(this, arguments);
+        }
+        if (child.prototype._init) {
+            child.prototype._init.apply(this, arguments);
+        }
+    };
 
     F.prototype = parent.prototype;
     child.prototype = new F();
