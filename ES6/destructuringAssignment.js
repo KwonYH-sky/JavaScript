727eb8bd29a5599070b730dd25c6f7835b20019a
KwonYH <yhoung11@gmail.com>
2023-01-20 21:48:12 +0900

    중첩 구조 분해 & 똑똑한 함수 매개변수

--- ES6/destructuringAssignment.js
+++ ES6/destructuringAssignment.js
@@ -262,6 +262,200 @@ let { title } = options;
 alert(title); // Menu
 */
 
-/** 
+/** 나머지 패턴 '...'
+ * 나머지 패턴(rest pattern)을 사용하면 배열에서 했던 것철머 나머지 프로퍼티를 어딘가에 할당하는게 가능하다.
+ * 참고로 모던 브라우저는 나머지 패턴을 지원하지만, IE를 비롯한 몇몇 구식 브라우저는 나머지 패턴을 지원하지 않으므로 주의해서 사용해야한다.
+ * 물론 바벨(Babel)을 이용하면 사용가능하다.
+ */
+
+options = {
+    title: "Menu",
+    width: 100,
+    height: 200
+};
+
+// title = 이름이 title인 프로퍼티
+// rest = 나머지 프로퍼티들
+({ title, ...rest } = options);
+
+// title엔 "Menu", rest엔 {height: 200, width: 100}이 할당된다.
+alert(rest.height); // 200
+alert(rest.width); // 100
+
+/** let 없이 사용하기
+ * 할당 연산 let {...} = {...} 안에서 변수들을 선언하였다. 
+ * let으로 새로운 변수를 선언하지 않고 기존에 있던 변수에 분해한 값을 할당할 수도 있는데, 이때 주의해야한다.
  * 
+
+let title, width, height;
+
+//  SyntaxError: Unexpected token '='  이라는 에러가 아랫줄에서 발생한다.
+{title, width, height} = {title:"Menu", width:200, height:100};
+
+ * 자바스크립트는 표현식 안에 있지 않으면서 주요 코드 흐름 상에 있는 {...}를 코드 블록으로 인식한다.
+ * 코드 블록의 본래 용도는 아래와 같이 문(statement)을 묶는 것이다.
+
+{
+    // 코드 블록
+    let message = "Hello";
+    // ...
+    alert(message);
+}
+
+ * 위쪽 예시에선 구조 분해 할당을 위해 사용한 {...}를 자바스크립트가 코드 블록으로 인식해서 에러가 발생하였다.
+ * 에러를 해결하기 위해선 할당문을 괄호(...)로 감싸 자바스크립트가 {...}를 코드 블록이 아닌 표현식으로 해석하게 하면 된다.
+ 
+let title, width, height;
+
+// 에러가 발생하지 않는다.
+({title, width, height} = {title:"Menu", width:200, height:100});
+
+alert(title); // Menu
+ */
+
+///////////////////////////////////////////////////
+
+/** 중첩 구조 분해
+ * 객체나 배열이 다른 객체나 배열을 포함하는 경우, 
+ * 좀 더 복잡한 패턴을 사용하면 중첩 배열이나 객체의 정보를 추출할 수 있다.
+ * 이를 중첩 구조 분해(nested destructuring)라고 부른다.
+ * 
+ * 아래 예시에서 객체 options의 size 프로퍼티 값은 또 다른 객체이다. 
+ * items 프로퍼티는 배열을 값으로 가지고 있다.
+ * 대입 연산자 좌측 패턴은 정보를 추출하려는 객체 options와 같은 구조를 갖추고 있다.
+ */
+
+options = {
+    size: {
+        width: 100,
+        height: 200
+    },
+    items: ["Cake", "Donut"],
+    extra: true
+};
+
+// 코드를 여러 줄에 걸쳐 작성해 의도하는 바를 명확히 드러냄
+({
+    size: { // size는 여기
+        width,
+        height
+    },
+    items: [item1, item2], // items는 여기에 할당함
+    title: "Menu" // 분해하려는 객체에 title 프로퍼티가 없으므로 기본값을 사용함
+} = options);
+
+alert(title); // Menu
+alert(width); // 100
+alert(height); // 200
+alert(item1); // Cake
+alert(item2); // Donut
+
+/* extra(할당 연산자 좌측의 패턴에는 없음)을 제외한 options 객체의 모든 프로퍼티가 상응하는 변수에 할당되었다.
+
+   변수 width, height, item1, item2엔 원하는 값이 title엔 기본값이 저장되었다.
+   그런데 위 예시에서 size와 items 전용 변수는 없다는 점에 유의하자.
+   전용 변수 대신 우리는 size와 items 안의 정보를 변수에 할당하였다.
+*/
+
+/////////////////////////////////////////////////////////
+
+/** 똑똑한 함수 매개변수
+ * 함수에 매개변수가 많은데 이중 상당수는 선택적으로 쓰이는 경우가 종종 있다.
+ * 사용자 인터페이스와 연관된 함수에서 이런 상황을 자주 볼수 있다.
+ * 메뉴 생성에 관여하는 함수가 있다고 가정해보자.
+ * 메뉴엔 너비, 높이, 제목, 항목 리스트 등이 필요하기 때문에 이 정보는 매개변수로 받는다.
+ * 
+ * 먼저 리팩토링 전의 메뉴 생성 함수를 보자
+
+function showMenu(title = "Untitled", width = 200, height = 100, items = []) {
+    // ...
+}
+
+ * 이렇게 함수를 작성하면 넘겨주는 인수의 순서가 틀려 문제가 발생할 수 있다.
+ * 문서화가 잘 되어있다면 IDE가 순서를 틀리지 않게 도움을 주긴 하곘지만,
+ * 이 외에도 대부분의 매개변수에 기본값이 설정되어 있어 굳이 인수를 넘겨주지 않아도 되는 경우에 문제가 발생한다.
+
+// 기본값을 사용해도 괜찮은 경우 아래와 같이 undefined를 여러 개 넘겨줘야 한다.
+showMenu("MyMenu", undefined, undefined, ["Item1", "Item2"])
+
+ * 깨끗해 보이지 않는다. 매개변수가 많아질수록 가독성은 더 떨어질 것이다.
+ * 구조 분해는 이럴 때 좋은 해결 방안이 될거다.
+ * 
+ * 매개변수 모두를 객체에 모아 함수에 전달해, 
+ * 함수가 전달받은 객체를 분해하여 변수에 할당하고 
+ * 원하는 작업을 수행할 수 있도록 함수를 리팩토링 해보자
+ */
+
+// 함수에 전달할 객체
+options = {
+    title: "My menu",
+    items: ["Item1", "Item2"]
+};
+
+// 똑똑한 함수는 전달받은 객체를 분해해 변수에 즉시 할당함
+function showMenu({ title = "Untitled", width = 200, height = 100, items = [] }) {
+    // title, items - 객체 options에서 가져옴
+    // width. height - 기본값
+    alert(`${title} ${width} ${height}`); // My menu 200 100
+    alert(items); // Item1, Item2
+}
+
+showMenu(options);
+
+/* 중쳡 객체와 클론을 조합하면 좀 더 복잡한 구조 분해도 가능하다 */
+
+function showMenu({
+    title = "Untitled",
+    width: w = 200, // width는 w에,
+    height: h = 100, // height는 h에,
+    items: [item1, item2] // items의 첫 번째 요소는 item1에, 두 번째 요소는 item2에 할당함
+}) {
+    alert(`${title} ${w} ${h}`); // My menu 200 100
+    alert(item1); // Item1
+    alert(item2); // Item2
+}
+
+showMenu(options);
+
+/* 이렇게 똑똑한 함수 매개변수 문법은 구조 분해 할당 문법과 동일하다.
+
+function ({
+    incomingProperty: varName = defaultValue
+    ...
+})
+
+   매개변수로 전달된 객체의 프로퍼티 incomingProperty는 varName에 할당된다.
+   만약 값이 없다면 defaultValue가 기본값으로 사용될 것이다.
+
+   참고로 이렇게 함수 매개변수를 구조 분해할 땐, 반드시 인수가 전달된다고 가정하고 사용된다는 점에 유의하자
+   모든 인수에 기본값을 할당해 주려면 빈 객체를 명시적으로 전달해야한다.
+*/
+showMenu({}); // 모든 인수에 기본값이 할당된다.
+
+showMenu(); // 에러가 발생할 수 있다.
+
+/* 이 문제를 예방하려면 빈 객체 {}를 인수 전체의 기본값으로 만들면 된다. */
+function showMenu({ title = "Menu", width = 100, height = 200 } = {}) {
+    alert(`${title} ${width} ${height}`);
+}
+
+showMenu(); // Menu 100 200
+
+/* 이렇게 인수 객체의 기본값을 빈 객체 {}로 설정하면
+   어떤 경우든 분해 할 것이 생겨서 함수에 인수를 하나도 전달하지 않아도 에러가 발생하지 않는다.
+*/
+
+/////////////////////////////////////////
+
+/** 요약
+  * 구조 분해 할당을 사용하면 객체나 배열을 변수로 연결할 수 있다.
+  * 객체 분해하기:
+  let {prop : varName = default, ...rest} = object
+  object의 프로퍼티 prop의 값은 변수 varName에 할당되는데, object에 prop이 없으면 default가 varName에 할당된다.
+  연결할 변수가 없는 나머지 프로퍼티들은 객체 rest에 복사된다.
+
+  * 배열 분해하기:
+  let [item1 = default, item2, ...rest] = array
+  array의 첫 번째 요소는 item1에, 두 번째 요소는 변수 item2에 할당되고, 이어지는 나머지 요소들은 배열 rest에 저장된다.
+  * 할당 연산다 좌측의 패턴과 우측의 구조가 같으면 중첩 배열이나 객체가 있는 복잡한 구조에서도 원하는 데이터를 뽑아낼 수 있다.
  */
