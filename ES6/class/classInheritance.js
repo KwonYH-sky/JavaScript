3d244aa702d047c5b8cbf5af67803c750ee148a8
KwonYH <yhoung11@gmail.com>
2023-01-30 22:35:59 +0900

    constructor overriding

--- ES6/class/classInheritance.js
+++ ES6/class/classInheritance.js
@@ -158,3 +158,102 @@ rabbit.stop(); // 흰 토끼가 멈췄습니다. 흰 토끼가 숨었습니다.
 
 /////////////////////////////////////////////////////////
 
+/** 생성자 오버라이딩
+ * 생성자 오버라이딩은 좀 더 까다롭다.
+ * 지금까진 Rabbit에 자체 constructor가 없었다.
+ * 명세서에 따르면, 클래스가 다른 클래스를 상속받고 constructor가 없는 경우엔 아래처럼 '비어있는' constructor가 만들어진다.
+
+    class Rabbit extends Animal {
+        // 자체 생성자가 없는 클래스를 상속받으면 자동으로 만들어짐
+        constructor(...args) {
+            super(...args);
+        }
+    }
+
+ * 보시다시피 생성자는 기본적으로 부모 constructor를 호출한다.
+ * 이때 부모 constructor에도 인수를 모두 전달한다.
+ * 클래스에 자체 생성자가 없는 경우엔 이런 일이 모두 자동으로 일어난다.
+ * 
+ * 이제 Rabbit에 커스텀 생성자를 추가해보자. 커스텀 생성자에서 name과 earLength를 지정한다.
+ */
+
+class Rabbit extends Animal {
+    constructor(name, earLength) {
+        this.speed = 0;
+        this.name = name;
+        this.earLength = earLength;
+    }
+    // ...
+}
+
+// 동작하지 않는다.
+rabbit = new Rabbit("흰 토끼", 10); // ReferenceError: Must call super constructor in derived class before ...
+
+/* 에러가 발생해 토끼를 만들 수 없다.
+ * 이유는 다음과 같다
+     * 상속 클래스의 생성자에선 반드시 super(...)를 호출해야 하는데, super(...)를 호출하지 않아 에러가 발생했다.
+        super(...)는 this를 사용하기 전에 반드시 호출해야 한다.
+ * 그런데 왜 super(...)를 호출해야 할까?
+ * 상속 클래스의 생성자가 호출될 때 어떤 일이 일어나는지 알아보며 이유를 찾아보자.
+ * 
+ * 자바스크립트는 '상속 클래스의 생성자 함수(derived constructor)'와 그렇지 않는 생성자 함수를 구분한다.
+ * 상속 클래스의 생성자 함수엔 특수 프로퍼티인 [[ConstructorKind]]: "derived"가 이름표처럼 붙는다.
+ * 
+ * 일반 클래스의 생성자 함수와 상속 클래스의 생성자 함수 간 차이는 new와 함께 드러난다.
+     * 일반 클래스가 new와 함께 실행되면, 빈 객체가 만들어지고 this에 이 객체를 할당한다.
+     * 반면, 상속 클래스의 생성자 함수가 실행되면, 일반 클래스에서 일어난 일이 일어나지 않는다.
+        상속 클래스의 생성자 함수는 빈 객체를 만들고 this에 이 객체를 할당하는 일을 부모 클래스의 생성자가 처리해주길 기대한다.
+ * 이런 차이 때문에 상속 클래스의 생성자에선 super를 호출해 부모 생성자를 실행해 주어야 한다.
+ * 그렇지 않으면 this가 될 객체가 만들어지지 않아 에러가 발생한다.
+ * 
+ * 아래 예시와 같이 this를 사용하기 전에 super()를 호출하면 Rabbit의 생성자가 제대로 동작한다.
+ */
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+        this.speed = 0;
+    }
+    // ...
+}
+
+class Rabbit extends Animal {
+    constructor(name, earLength) {
+        super(name);
+        this.earLength = earLength;
+    }
+
+    // ...
+}
+
+// 이제 에러 없이 동작한다.
+rabbit = new Rabbit("흰 토끼", 10);
+alert(rabbit.name); // 흰 토끼
+alert(rabbit.earLength); // 10
+
+/** 클래스 필드 오버라이딩: 까다로운 내용
+ * 오버라이딩은 메서드뿐만 아니라 클래스 필드를 대상으로도 적용할 수 있다.
+ * 부모 클래스의 생성자 안에 있는 오버라이딩한 필드에 접근하려고 할 때 
+ * 자바스크립트는 다른 프로그래밍 언어와는 다르게 조금 까다롭다.
+ */
+
+class Animal {
+    name = 'animal';
+
+    constructor() {
+        alert(this.name); // (*)
+    }
+}
+
+class Rabbit extends Animal {
+    name = 'rabbit';
+}
+
+new Animal(); // animal
+new Rabbit(); // animal
+
+/* Animal을 상속받는 Rabbit에서 name 필드를 오버라이딩 했다.
+ * Rabbit에는 따로 생성자가 정의되어 있지 않기 때문에 
+ * Rabbit을 사용해 인스턴스를 만들면 Animal의 생성자가 호출된다.
+ * 
+ */
