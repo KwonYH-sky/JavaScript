d8235a02d26cca39c4c375b47d0b0e6a65c024f0
KwonYH <yhoung11@gmail.com>
2023-03-15 18:21:43 +0900

    await keyword

--- ES6/promise&async,await/async&await.js
+++ ES6/promise&async,await/async&await.js
@@ -35,3 +35,93 @@ f().then(alert); // 1
 
 /////////////////////////////
 
+/** await
+ * await 문법은 다음과 같다.
+    // await는 async 함수 안에서만 동작한다.
+    let value = await promise;
+ * 자바스크립트는 await 키워드를 만나면 프라미스가 처리될 때까지 기다린다(await는 '기다리다'라는 뜻을 가진 영단어이다).
+ * 결과는 그 이후 반환된다.
+ * 
+ * 1초 후 이행되는 프라미스를 예시로 사용하여 await가 어떻게 동작하는지 알아보자.
+ */
+async function f() {
+
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("완료!"), 1000)
+    });
+
+    let result = await promise; // 프라미스가 이행될 때까지 기다림 (*)
+
+    alert(result); // "완료!"
+}
+
+f();
+
+/* 함수를 호출하고, 함수 본문이 실행되는 도중에 
+ * (*)로 표시한 줄에서 실행이 잠시 '중단'되었다가 프라미스가 처리되면서 실행이 재개된다.
+ * 이때 프라미스 객체의 result 값이 변수 result에 할당된다. 따라서 위 예시를 실행하면 1초 후 '완료!'가 출력된다.
+ * 
+ * await는 말 그대로 프라미스가 처리될 때까지 함수 실행을 기다리게 만든다.
+ * 프라미스가 처리되면 그 결과와 함께 실행이 재개된다.
+ * 프라미스가 처리되길 기다리는 동안엔 엔진이 다른 일(다른 스크립트를 실행, 이벤트 처리 등)을 할 수 있기 때문에, CPU 리소스가 낭비되지 않는다.
+ * 
+ * await는 promise.then보다 좀 더 세련되게 프라미스의 result 값을 얻을 수 있도록 해주는 문법이다.
+ * promise.then보다 가독성이 좋고 쓰기 쉽다.
+ */
+
+/** ! 일반 함수엔 await을 사용할 수 없다.
+ * async 함수가 아닌데 await을 사용하면 문법 에러가 발생한다.
+ */
+function f() {
+    let promise = Promise.resolve(1);
+//    let result = await promise; // Syntax Error
+}
+/* function 앞에 async를 붙이지 않으면 이런 에러가 발생할 수 있다.
+ * 앞서 언급한 바와 같이 await는 async 함수 안에서만 동작한다.
+ */
+
+/* 이전 showAvatar() 예시를 async/await를 사용해 다시 작성해보자.
+    * 1. 먼저 .then 호출 await로 바꾸어야 한다.
+    * 2. function 앞에 async를 붙여 await를 사용할 수 있도록 해야 한다.
+ */
+async function showAvatar() {
+    
+    // JSON 읽기
+    let response = await fetch('/article/promise-chaining/user.json');
+    let user = await response.json();
+
+    // github 사용자 정보 읽기
+    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+    let githubUser = await githubResponse.json();
+
+    // 아바타 보여주기
+    let img = document.createElement('img');
+    img.src = githubUser.avatar_url;
+    img.className = "promise-avatar-example";
+    document.body.append(img);
+
+    // 3초 대기
+    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+    img.remove();
+
+    return githubUser;
+}
+
+showAvatar();
+/* 코드가 깔끔해지고 읽기도 쉬워졌다. 프라미스를 사용하는 것보다 가독성이 좋아졌다. */
+
+/** i await는 최상위 레벨 코드에서 작동하지 않는다.
+ * await을 이제 막 사용하기 시작하면 최상위 레벨 코드(top-level-code)에서 await을 사용할 수 없다는 잊는다.
+ * 아래와 같은 코드는 동작하지 않는다.
+    // 최상위 레벨 코드에선 문법 에러가 발생함
+    let response = await fetch('/article/promise-chaining/user.json');
+    let user = await response.json();
+ * 하지만 익명 async 함수로 코드를 감싸면 최상위 레벨 코드에도 await를 사용할 수 있다.
+ */
+(async () => {
+    let response = await fetch('/article/promise-chaining/user.json');
+    let user = await response.json();
+    // ....
+})();
+
